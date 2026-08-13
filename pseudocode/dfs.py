8a11de00542e004e2ba6377b34e6adcb213829e9
davidhilscher <davidhilscher13@gmail.com>
2026-08-13 00:42:32 -0400

    organizing

--- pseudocode/dfs.py
+++ pseudocode/dfs.py
@@ -1,9 +1,3 @@
-# eddy edmonds Vtech
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
 class TreeNode:
     def __init__(self,x):
